a1329ed202ab9704993723ec80c94b2760cfeded
sana-manzoor <144760083+sana-manzoor@users.noreply.github.com>
2025-07-12 13:07:40 +0530

    committedddd

--- src/services/baseurl.js
+++ src/services/baseurl.js
@@ -1,3 +1,3 @@
-// export const BASE_URL="http://localhost:4000"
-export const BASE_URL="https://ecoomerce-server-intmainproj-1.onrender.com"
+ export const BASE_URL="http://localhost:4000"
+//export const BASE_URL="https://ecoomerce-server-intmainproj-1.onrender.com"
 
